f09439ff7e4e9ca331d11aa59cd22834715cef07
SCP3812 Wojak <mrisaacjgraham@gmail.com>
2024-03-11 21:20:55 +0000

    finalized

---
+++ genetic.py
@@ -0,0 +1,26 @@
+GOAL = 50000
+NUM_MEN = 20 
+INITIAL_MIN_WT = 200
+INITIAL_MAX_WT = 600
+INITIAL_MODE_WT = 300 
+MUTATE_ODDS = 0.01
+MUTATE_MIN = 0.5
+MUTATE_MAX = 1.2
+FAMILY_SIZE = 8
+FAMILIES_PER_YEAR = 10
+GENERATION_LIMIT = 500
+
+def populate(num_rats, min_wt, max_wt, mode_wt):
+    
+def fitness(population, goal):
+
+def selector(population, to_retain, truf):
+
+def breed(males, females, family_size):
+
+def mutate(children, mutate_odds, mutate_min, mutate_max)
+
+def main():
+
+
+
