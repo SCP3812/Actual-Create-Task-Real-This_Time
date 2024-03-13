f7ab2634a51228975516c6faea0a9b7ad50cb434
SCP3812 Wojak <mrisaacjgraham@gmail.com>
2024-03-13 21:27:08 +0000

    sus

--- genetic.py
+++ genetic.py
@@ -18,13 +18,12 @@ FAMILIES_PER_YEAR = 10
 GENERATION_LIMIT = 500
 
 class human:
-    def __init__(name, weight)
-        self.name = name
+    def __init__(self, weight):
         self.weight = weight
-    def create(a,b,c):
+    def create(self,a,b,c):
         fc = (c - a)/(b - a)
-        u = random()
-        var x = 0
+        u = random.random()
+        x = 0
         if u < fc:
             x = (b-a) * (c-a)
             self.weight = math.floor(a + math.sqrt(x*u))
@@ -43,7 +42,7 @@ def populate(num_men, min_wt, max_wt, mode_wt):
     parents = []
     n = 0
     while n < num_men:
-        man = human(n, min_wt)
+        man = human(min_wt)
         parents.append(man)
         n+=1
     n1 = 0
@@ -53,24 +52,26 @@ def populate(num_men, min_wt, max_wt, mode_wt):
     
 def fitness(population, goal):
     place_hold = []
-    for ppl in population
-        place_hold.append(population[i].weight)
+    for ppl in population:
+        place_hold.append(ppl.weight)
+        print(ppl.weight)
+    print(place_hold)
     ave = statistics.mean(place_hold)
     return ave/goal
 
 def selector(population, to_retain, truf):
-    sorted_population = sorted(population)
+    sorted_population = sorted(population, key = lambda x: x.weight, reverse=True)
     if len(sorted_population) > to_retain:
         sorted_population = sorted_population.slice(-to_retain)
     to_retain_by_sex = math.floor(to_retain/2)
     members_per_sex = math.floor(len(sorted_population)/2)
     females = sorted_population[0:members_per_sex]
     males = sorted_population[members_per_sex:len(sorted_population)]
-    selected_females = females.slices(-to_retain_by_sex)
-    selected_males = males.slice(-to_retain_by_sex)
-    if truf == true:
+    selected_females = females[to_retain_by_sex:len(females)]
+    selected_males = males[to_retain_by_sex:len(males)]
+    if truf == True:
         return selected_females
-    elif truf == false:
+    elif truf == False:
         return selected_males
 
 def breed(males, females, family_size):
@@ -89,7 +90,7 @@ def breed(males, females, family_size):
             children.append(child)
     return children
 
-def mutate(children, mutate_odds, mutate_min, mutate_max)
+def mutate(children, mutate_odds, mutate_min, mutate_max):
     n=0
     while n < len(children):
         n+=1
@@ -100,31 +101,32 @@ def mutate(children, mutate_odds, mutate_min, mutate_max)
     return children
 
 generations = 0
-parents = populate(NUM_RATS, INITIAL_MIN_WT, INITIAL_MAX_WT, INITIAL_MODE_WT)
+parents = populate(NUM_MEN, INITIAL_MIN_WT, INITIAL_MAX_WT, INITIAL_MODE_WT)
 init_pop_wts = []
 
 n=0
 while n < NUM_MEN:
-    n+=1
     init_pop_wts.append(parents[n].weight)
+    n+=1
+    
 
-print("initial population weights = " + init_pop_wts)
+print("initial population weights = " + str(init_pop_wts))
 popl_fitness = fitness(parents, GOAL)
-print("initial population fitness = " + popl_fitness)
-print("number to retain = " + NUM_MEN)
+print("initial population fitness = " + str(popl_fitness))
+print("number to retain = " + str(NUM_MEN))
 
 ave_wt = []
 
 while popl_fitness < 1 and generations < GENERATION_LIMIT:
-    selected_females = selector(parents, NUM_RATS, true)
-    selected_males = selector(parents, NUM_RATS, false)
-    children = breed(selected_males, selected_females, LITTER_SIZE)
+    selected_females = selector(parents, NUM_MEN, True)
+    selected_males = selector(parents, NUM_MEN, False)
+    children = breed(selected_males, selected_females, FAMILY_SIZE)
     weight_hold = []
     children = mutate(children, MUTATE_ODDS, MUTATE_MIN, MUTATE_MAX)
     place_hold = selected_males + selected_females
     parents = place_hold + children
-        for i in parents:
-            weight_hold.append(i.weight)
+    for i in parents:
+        weight_hold.append(i.weight)
     popl_fitness = fitness(parents, GOAL)
     print("Generation " + generations + " fitness: " + popl_fitness)
     ave_wt.append(math.floor(statistics.mean(weight_hold)))
